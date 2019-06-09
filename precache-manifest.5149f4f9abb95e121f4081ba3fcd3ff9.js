4e335ac92cb05eefcb3e06f2a42e223162f57354
AJ Adversalo <ajadvers@gmail.com>
2019-06-08 21:43:54 -0700

    Updates

---
+++ precache-manifest.5149f4f9abb95e121f4081ba3fcd3ff9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbde999f76687a2f3090030d1aefe5d",
+    "url": "/netflix-navigator/index.html"
+  },
+  {
+    "revision": "d356b1d703e9ab712aa2",
+    "url": "/netflix-navigator/static/css/2.36c7e612.chunk.css"
+  },
+  {
+    "revision": "df92149b870525afe944",
+    "url": "/netflix-navigator/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "d356b1d703e9ab712aa2",
+    "url": "/netflix-navigator/static/js/2.71be3c5a.chunk.js"
+  },
+  {
+    "revision": "df92149b870525afe944",
+    "url": "/netflix-navigator/static/js/main.b875d53f.chunk.js"
+  },
+  {
+    "revision": "1044a7f82b1c62d7a58c",
+    "url": "/netflix-navigator/static/js/runtime~main.3cefd054.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/netflix-navigator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/netflix-navigator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/netflix-navigator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/netflix-navigator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/netflix-navigator/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix-navigator/precache-manifest.f9fe63608c05ee78dc1ed0b6907284c7.js"
+  "/netflix-navigator/precache-manifest.5149f4f9abb95e121f4081ba3fcd3ff9.js"
 );
 
 self.addEventListener('message', (event) => {
